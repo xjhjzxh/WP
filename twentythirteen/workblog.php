e3608c0202324637323425b529a8d0dd1599aa1e
xjhjzxh <234957111@qq.com>
2016-09-27 16:24:25 +0800

    php file

---
+++ twentythirteen/workblog.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages and that other
+ * 'pages' on your WordPress site will use a different template.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Thirteen
+ * @since Twenty Thirteen 1.0
+ */
+/*
+Template Name:worklog
+*/
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
